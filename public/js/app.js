caf623c8ecbbed5a5fd4d70b4771887b6e436699
zacharyhalvorson <zacharyhalvorson@me.com>
2014-11-27 21:41:17 -0800

    Think I might have figured out my logo

--- public/js/app.js
+++ public/js/app.js
@@ -16,13 +16,3 @@ $('.m-scooch').on('afterSlide', function(e, previousSlide, nextSlide) {
         $('.m-scooch-controls [data-m-slide="next"]').addClass('hide');
     };
 });
-
-// Logo animation
-// $('.logo a').hover( function(){
-//
-//     // alert('hover');
-//     $('#top').velocity({translateY: '-10px'});
-//     // $('#middle').velocity({rotate:'-45deg'});
-//     $('#bottom').velocity({translateY:'30px'});
-//
-// });
