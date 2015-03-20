885aa8d835f51a9ad6877b1b365a69100cbe3d92
Zachary Halvorson <zacharyhalvorson@me.com>
2015-03-19 18:38:53 -0700

    more commits?

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,25 @@
-$('.m-scooch').scooch();
+$( '.m-scooch' ).scooch();
 
-if ( $('.m-item:first-child' ).hasClass('m-active') ) {
-    $('.m-scooch-controls [data-m-slide="prev"]').addClass('hide');
+if ( $( '.m-item:first-child' ).hasClass( 'm-active' ) ) {
+
+    $( '.m-scooch-controls [data-m-slide="prev"]' ).addClass( 'hide' );
+
 };
 
-$('.m-scooch').on('afterSlide', function(e, previousSlide, nextSlide) {
+$( '.m-scooch' ).on( 'afterSlide', function(e, previousSlide, nextSlide) {
 
-    $('.m-scooch-controls a').removeClass('hide');
+    $( '.m-scooch-controls a' ).removeClass( 'hide' );
 
-    if ( $('.m-item:first-child' ).hasClass('m-active') ) {
+    if ( $('.m-item:first-child' ).hasClass( 'm-active' ) ) {
+
         $('.m-scooch-controls [data-m-slide="prev"]').addClass('hide');
+
     };
 
-    if ( $('.m-item:last-child' ).hasClass('m-active') ) {
-        $('.m-scooch-controls [data-m-slide="next"]').addClass('hide');
+    if ( $('.m-item:last-child' ).hasClass( 'm-active' ) ) {
+
+        $( '.m-scooch-controls [data-m-slide="next"]' ).addClass( 'hide' );
+
     };
+
 });
