035eeac12384b163ed616529d020e4a2a74bf0c9
Zachary Halvorson <zacharyhalvorson@me.com>
2017-09-17 16:28:07 -0700

    Spin up Gatsby site

---
+++ .cache/async-requires.js
@@ -0,0 +1,23 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/zachary/Repos/personal-site/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/zachary/Repos/personal-site/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/zachary/Repos/personal-site/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Repos/personal-site/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/zachary/Repos/personal-site/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Repos/personal-site/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/zachary/Repos/personal-site/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Repos/personal-site/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/zachary/Repos/personal-site/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Repos/personal-site/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/zachary/Repos/personal-site/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/zachary/Repos/personal-site/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/zachary/Repos/personal-site/src/layouts/index.js"
+  import data from "/Users/zachary/Repos/personal-site/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
