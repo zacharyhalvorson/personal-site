29bddfaa9803ad3a39c4d5ceaa7dcf157ec76edc
Zachary Halvorson <zacharyhalvorson@me.com>
2018-09-10 18:40:52 -0700

    add new social images

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,15 +2,12 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/zachary/Documents/GitHub/personal-site/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/zachary/Documents/GitHub/personal-site/src/pages/404.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/zachary/Documents/GitHub/personal-site/src/pages/index.js"),
   "component---src-pages-resume-js": require("gatsby-module-loader?name=component---src-pages-resume-js!/Users/zachary/Documents/GitHub/personal-site/src/pages/resume.js")
 }
 
 exports.json = {
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Documents/GitHub/personal-site/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/zachary/Documents/GitHub/personal-site/.cache/json/dev-404-page.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Documents/GitHub/personal-site/.cache/json/layout-index.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/zachary/Documents/GitHub/personal-site/.cache/json/404.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/zachary/Documents/GitHub/personal-site/.cache/json/layout-index.json"),
