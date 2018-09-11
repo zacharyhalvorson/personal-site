122ef2be917685661f2f713a6c7c096dfa8735e4
Zachary Halvorson <zacharyhalvorson@me.com>
2018-09-10 18:32:53 -0700

    reskin

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
-import Me from '../assets/images/me.png'
+import Me from '../assets/images/me.jpg'
 
 const BREAK_ONE = '40rem';
 
 const PageTitle = styled.h1`
 	font-weight: 900;
-	text-transform: uppercase;
 	position: relative;
 	z-index: 2;
 	border-bottom: none;
 
 	@media (min-width: ${BREAK_ONE}) {
 		font-size: 3rem;
+    margin-bottom: 4rem;
 	}
 `
 
@@ -37,7 +37,6 @@ const MyPhoto = styled.img`
 	z-index: 1;
 	margin: 0 auto;
 	width: 60%;
-	opacity: 0.7;
 
 	@media (min-width: ${BREAK_ONE}) {
 		float: left;
@@ -65,7 +64,7 @@ const IndexPage = () => (
 			<p>I’ve worked on both web and native applications, and relish an opportunity to see a product through from research to implementation.</p>
 			<p>Currently, I lead design at <a target="_blank" href="https://dooly.ai">Dooly</a>. We make a notetaking app that helps people sell.</p>
 			<p>As a product designer at <a target="_blank" href="https://mobify.com">Mobify</a>, I helped develop their Progressive Web App framework, launched a marketing engagement tool called Connection Center, and did UI design and development for several native app projects including <a target="_blank" href="https://itunes.apple.com/ca/app/ritchie-bros./id1068567213">Ritchie Bros.</a> for iOS and Android.</p>
-			<p>Feel free to get in touch! 👋</p>
+			<p>Feel free to get in touch!</p>
 			<a href="mailto:hello@zacharyhalvorson.com">hello@zacharyhalvorson.com</a>
 		</Text>
   </ContentWrapper>
