2f40c888cf3069a1a8324fd30cd44bc6b99e9f01
Zachary Halvorson <zacharyhalvorson@me.com>
2018-09-10 18:50:50 -0700

    Add some fresh copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,8 +61,7 @@ const IndexPage = () => (
 		<MyPhoto src={Me} alt="A photo of me." title="Nice to meet you!" />
 
 		<Text>
-			<p>I’ve worked on both web and native applications, and relish an opportunity to see a product through from research to implementation.</p>
-			<p>Currently, I lead design at <a target="_blank" href="https://dooly.ai">Dooly</a>. We make a notetaking app that helps people sell.</p>
+      <p>Currently, I lead design at <a target="_blank" href="https://dooly.ai">Dooly</a>. We’re a note taking and productivity tool to help customer-facing people have better meetings. I also teach Web Development at <a target="_blank" href="https://brainstation.io">BrainStation</a> once a week in the evenings.</p>
 			<p>As a product designer at <a target="_blank" href="https://mobify.com">Mobify</a>, I helped develop their Progressive Web App framework, launched a marketing engagement tool called Connection Center, and did UI design and development for several native app projects including <a target="_blank" href="https://itunes.apple.com/ca/app/ritchie-bros./id1068567213">Ritchie Bros.</a> for iOS and Android.</p>
 			<p>Feel free to get in touch!</p>
 			<a href="mailto:hello@zacharyhalvorson.com">hello@zacharyhalvorson.com</a>
