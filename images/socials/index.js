8444c2c1ac88984b75691c82fe3d41c18c57c8f7
Zachary Halvorson <zacharyhalvorson@me.com>
2020-02-16 12:56:20 -0800

    Add new simpler static site without all the JS nonsense...

---
+++ images/socials/index.js
@@ -0,0 +1,11 @@
+import Github from './github.svg'
+import Dribbble from './dribbble.svg'
+import Twitter from './twitter.png'
+import AppleMusic from './apple-music.jpg'
+
+export default {
+  Github,
+  Dribbble,
+  Twitter,
+  AppleMusic,
+}
